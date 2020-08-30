a5d9b9ab2ebe5839ec6203b2f50ef163c4f61734
piaoger <piaoger@yahoo.com>
2020-08-30 21:02:55 +0800

    chore:minor modifications on example

--- examples/scraper.rs
+++ examples/scraper.rs
@@ -10,30 +10,28 @@ use std::io::prelude::*;
 // TODO:
 //  1. try to integrate with html2md in the future
 //     https://github.com/Adonai/html2md
-//  2. seems neight readability and html2md nor are very robust
+//  2. seems neither readability nor html2md are very robust
 //
 
 fn main() {
     let url = "https://blog.logrocket.com/rust-serialization-whats-ready-for-production-today/";
     match extractor::scrape(&url) {
         Ok(product) => {
-            println!("------- html ------");
-            println!("{}", product.content);
-
-            println!("---- plain text ---");
-            println!("{}", product.text);
-
             {
+                println!("------- html ------");
+                println!("{}", product.content);
+
                 let result = &product.content;
                 let mut file = fs::File::create("result.html").unwrap();
-
                 file.write_all(result.as_bytes()).unwrap();
             }
 
             {
+                println!("---- plain text ---");
+                println!("{}", product.text);
+
                 let result = &product.text;
                 let mut file = fs::File::create("result.txt").unwrap();
-
                 file.write_all(result.as_bytes()).unwrap();
             }
         }
